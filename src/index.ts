709aa028b0a2a66b007f139af0e7f4881e7a5867
hunghg255 <giahunghust@gmail.com>
2023-09-03 09:56:05 +0700

    initial

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// import { mkdirP } from '@actions/io'
+import { getInput, setFailed } from '@actions/core'
+
+function main() {
+  try {
+    const github_token: string = getInput('github_token', { required: false }).trim()
+    const from_branch = getInput('from_branch', { required: false })
+    const to_branch = getInput('to_branch', { required: true })
+
+    console.log(`github_token: ${github_token}`);
+    console.log(`from_branch: ${from_branch}`);
+    console.log(`to_branch: ${to_branch}`);
+
+  }
+  catch (error: any) {
+    setFailed(error.message)
+  }
+}
+
+//@ts-ignore
+main().catch(error => setFailed(error.message))
